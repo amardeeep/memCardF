3728921e471cea98311a2ce32915fc9f6134b969
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-25 21:55:51 +0530

    remove header and correct import paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-//import { useState } from 'react'
-import './App.css'
+import Card from './card.jsx'
+import Score from './score.jsx'
+import '/home/deetsy4455/repos/memCardF/src/styles/App.css'
 
 function App() {
+  
   return(
     <div className='app'>
-      
+      <h1>Test Your Memory</h1>
+      <Score></Score>
+      <div className="cards">
+        <Card></Card>
+      </div>
     </div>
   )
 }
